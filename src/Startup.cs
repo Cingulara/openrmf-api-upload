0b0991a4579ef4edd3a418584219da5896fcb798
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2020-01-11 13:23:58 -0500

    Adding Jaeger Tracing

--- src/Startup.cs
+++ src/Startup.cs
@@ -9,8 +9,13 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Swashbuckle.AspNetCore.Swagger;
 using Prometheus;
+using OpenTracing;
+using OpenTracing.Util;
+using Jaeger;
+using Jaeger.Samplers;
 
 using openrmf_upload_api.Models;
 using openrmf_upload_api.Data;
@@ -38,6 +43,28 @@ namespace openrmf_upload_api
                 options.ConnectionString = Environment.GetEnvironmentVariable("MONGODBCONNECTION");
                 options.Database = Environment.GetEnvironmentVariable("MONGODB");
             });
+            
+            // Use "OpenTracing.Contrib.NetCore" to automatically generate spans for ASP.NET Core
+            services.AddSingleton<ITracer>(serviceProvider =>  
+            {  
+                string serviceName = System.Reflection.Assembly.GetEntryAssembly().GetName().Name;  
+            
+                ILoggerFactory loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();  
+            
+                ISampler sampler = new ConstSampler(sample: true);  
+            
+                ITracer tracer = new Tracer.Builder(serviceName)  
+                    .WithLoggerFactory(loggerFactory)  
+                    .WithSampler(sampler)  
+                    .Build();  
+            
+                GlobalTracer.Register(tracer);  
+            
+                return tracer;  
+            });
+            services.AddOpenTracing();
+
+            // add repositories
             
             // Create a new connection factory to create a connection.
             ConnectionFactory cf = new ConnectionFactory();
