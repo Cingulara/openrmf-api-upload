91fed8ddd4cbd7be3c2dbd1aea0efc8142e11e82
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2020-01-26 16:21:16 -0500

    upload good and bad checklists tracking

---
+++ src/Models/UploadResult.cs
@@ -0,0 +1,19 @@
+// Copyright (c) Cingulara LLC 2019 and Tutela LLC 2019. All rights reserved.
+// Licensed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+
+namespace openrmf_upload_api.Models
+{
+    [Serializable]
+    public class UploadResult
+    {
+        public UploadResult () {
+            failedUploads = new List<string>();
+        }
+
+        public int successful { get; set; }
+        public int failed { get; set;}
+        public List<string> failedUploads { get; set;}
+    }
+}
