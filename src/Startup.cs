a3abd9fe485023bc55ba2a395afea3947a607c33
Dale Bingham <dalebingham@Dales-MBP.cingularanet>
2020-07-05 10:34:24 -0400

    Update for .NET Core 3.1

--- src/Startup.cs
+++ src/Startup.cs
@@ -10,7 +10,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Prometheus;
 using OpenTracing;
 using OpenTracing.Util;
@@ -36,11 +37,13 @@ namespace openrmf_upload_api
             var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
 
             // Register the database components
-            services.Configure<Settings>(options =>
-            {
-                options.ConnectionString = Environment.GetEnvironmentVariable("MONGODBCONNECTION");
-                options.Database = Environment.GetEnvironmentVariable("MONGODB");
-            });
+            if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DBTYPE")) || Environment.GetEnvironmentVariable("DBTYPE").ToLower() == "mongo") {
+                services.Configure<Settings>(options =>
+                {
+                    options.ConnectionString = Environment.GetEnvironmentVariable("DBCONNECTION");
+                    options.Database = Environment.GetEnvironmentVariable("DB");
+                });
+            }
             
             // Use "OpenTracing.Contrib.NetCore" to automatically generate spans for ASP.NET Core
             services.AddSingleton<ITracer>(serviceProvider =>  
@@ -106,13 +109,13 @@ namespace openrmf_upload_api
             // Register the Swagger generator, defining one or more Swagger documents
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "OpenRMF Upload API", Version = "v1", 
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "OpenRMF Upload API", Version = "v1", 
                     Description = "The Upload API that goes with the OpenRMF tool",
-                    Contact = new Contact
+                    Contact = new OpenApiContact
                     {
                         Name = "Dale Bingham",
                         Email = "dale.bingham@cingulara.com",
-                        Url = "https://github.com/Cingulara/openrmf-api-upload"
+                        Url = new Uri("https://github.com/Cingulara/openrmf-api-upload")
                     } });
             });
 
@@ -157,29 +160,14 @@ namespace openrmf_upload_api
                 options.AddPolicy("Assessor", policy => policy.RequireRole("roles", "[Assessor]"));
             });
 
-            // ********************
-            // USE CORS
-            // ********************
-            services.AddCors(options =>
-            {
-                options.AddPolicy("AllowAll",
-                    builder =>
-                    {
-                        builder
-                        .AllowAnyOrigin() 
-                        .AllowAnyMethod()
-                        .AllowAnyHeader()
-                        .AllowCredentials();
-                    });
-            });
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddControllers();
 
             // add this in memory for now. Persist later.
         	services.AddDistributedMemoryCache();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             // Custom Metrics to count requests for each endpoint and the method
             var counter = Metrics.CreateCounter("openrmf_upload_api_path_counter", "Counts requests to OpenRMF endpoints", new CounterConfiguration
@@ -214,13 +202,15 @@ namespace openrmf_upload_api
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "OpenRMF Upload API V1");
             });
 
-            // ********************
-            // USE CORS
-            // ********************
-            app.UseCors("AllowAll");
-            app.UseAuthentication();
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseRouting();
+            // this has to go here
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
